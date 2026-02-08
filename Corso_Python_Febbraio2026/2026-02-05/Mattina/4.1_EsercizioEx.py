d90b94eaae29a656fc3a666eb5a230bef6594ac0
dessirob92 <dessirob92@gmail.com>
2026-02-08 08:55:08 +0100

    Delete Corso_Python_Febbraio2026 directory

--- Corso_Python_Febbraio2026/2026-02-05/Mattina/4.1_EsercizioEx.py
+++
@@ -1,2 +0,0 @@
-# Andare a creare una lista che salva tutti i tentativi e un ultima sezione del
-# programma che permette di visionare o modificare la lista
--- Corso_Python_Febbraio2026/2026-02-05/Pomeriggio/7.1_Esercizi funzioni Ex.py
+++
@@ -1,2 +0,0 @@
-# Ex. Creare una funzione che converte da input a lista e una per convertire da lista a
-# tupla e viceversa
